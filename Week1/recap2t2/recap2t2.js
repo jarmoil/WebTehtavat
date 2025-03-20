800b2a0833d5fcbd30880727938174963c742a60
jarmoil <jarmoil@metropolia.fi>
2025-03-20 10:18:20 +0200

    Refactor recap2t2.js to use 'let' for numbers array and add search, pop, and sort functionalities

--- Week1/recap2t2/recap2t2.js
+++ Week1/recap2t2/recap2t2.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const numbers = [];
+let numbers = [];
 
 var i = 0;
 for(i=0; i<5; i++) {
@@ -12,5 +12,18 @@ numbers.forEach((item, index) => {
   console.log(`${index}: ${item}`)
 });
 
+console.log(`Numbers: [${numbers.join(", ")}]`)
 
+const num = +prompt('Enter a Number to Search:')
 
+if(numbers.includes(num))
+  console.log(`Number ${num} is found in the array.`)
+else
+  console.log(`Number ${num} is not found in the array.`)
+
+numbers.pop()
+console.log(`Updated Numbers: [${numbers.join(", ")}]`)
+
+numbers.sort(function(a,b){return a-b})
+
+console.log(`Sorted Numbers: [${numbers.join(", ")}]`)
