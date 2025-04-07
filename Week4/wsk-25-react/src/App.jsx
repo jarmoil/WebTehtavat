7bb59c88729aa497dc4b1d559846f41b7ea5d85a
jarmo <jarmoil@metropolia.fi>
2025-04-07 13:10:49 +0300

    Refactor App component to remove Greeting and add MediaRow component for media display

--- Week4/wsk-25-react/src/App.jsx
+++ Week4/wsk-25-react/src/App.jsx
@@ -1,13 +1,10 @@
 import './App.css';
 import Home from './components/Home';
-import Greeting from './Greeting';
 
 const App = () => {
   return (
     <>
       <h1>My App</h1>
-      <Greeting name="Jarmo" />
-      <Greeting name={123} />
       <Home />
     </>
   );
