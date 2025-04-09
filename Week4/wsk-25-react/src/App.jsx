746d18a22ba1d79f0ac8d71afc954b8e9275bfe7
jarmo <jarmoil@metropolia.fi>
2025-04-09 15:32:56 +0300

    Implement routing and layout structure; add media display and single view functionality

--- Week4/wsk-25-react/src/App.jsx
+++ Week4/wsk-25-react/src/App.jsx
@@ -1,12 +1,23 @@
+import {BrowserRouter, Route, Routes} from 'react-router';
 import './App.css';
-import Home from './components/Home';
+import Home from './views/Home';
+import Layout from './components/Layout';
+import Profile from './views/Profile';
+import Upload from './views/Upload';
+import Single from './views/Single';
 
-const App = () => {
+function App() {
   return (
-    <>
-      <h1>My App</h1>
-      <Home />
-    </>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/upload" element={<Upload />} />
+          <Route path="/single" element={<Single />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
-};
+}
 export default App;
