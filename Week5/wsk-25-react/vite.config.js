d30429cc1cfcac64cb95b84525dc1f5f47da964b
jarmo <jarmoil@metropolia.fi>
2025-04-14 10:41:53 +0300

    fix: update base path in Vite configuration for custom hooks

--- Week5/wsk-25-react/vite.config.js
+++ Week5/wsk-25-react/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/~jarmoil/Web-Sovelluskehitys/Week4/wsk-25-react/routing/',
+  base: '/~jarmoil/Web-Sovelluskehitys/Week5/custom-hooks/',
 });
