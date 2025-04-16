71b92e40dd208b901d86f05a40623a8a93bc9e46
jarmo <jarmoil@metropolia.fi>
2025-04-16 19:20:02 +0300

    feat: update upload functionality and routing, add media posting logic, and adjust README link

--- Week5/wsk-25-react/README.md
+++ Week5/wsk-25-react/README.md
@@ -1,4 +1,4 @@
-Open [Link](https://users.metropolia.fi/~jarmoil/Web-Sovelluskehitys/Week5/context) to view it in the browser.
+Open [Link](https://users.metropolia.fi/~jarmoil/Web-Sovelluskehitys/Week5/upload) to view it in the browser.
 
 # React + Vite
 
--- Week5/wsk-25-react/src/hooks/apiHooks.js
+++ Week5/wsk-25-react/src/hooks/apiHooks.js
@@ -2,6 +2,7 @@ import {useState} from 'react';
 import {uniqBy} from 'lodash';
 import {fetchData} from '../utils/fetchData';
 import {useEffect} from 'react';
+const mediaApiUrl = import.meta.env.VITE_MEDIA_API;
 
 const useMedia = () => {
   const [mediaArray, setMediaArray] = useState([]);
@@ -40,7 +41,24 @@ const useMedia = () => {
     getMedia();
   }, []);
 
-  return mediaArray;
+  const postMedia = async (file, inputs, token) => {
+    const data = {
+      ...inputs,
+      ...file,
+    };
+    const fetchOptions = {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer: ${token}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    };
+
+    return await fetchData(`${mediaApiUrl}/media`, fetchOptions);
+  };
+
+  return {mediaArray, postMedia};
 };
 
 const useAuthentication = () => {
@@ -82,12 +100,33 @@ const useUser = () => {
       },
     };
 
-    const userResult = await fetchData(
+    return await fetchData(
       import.meta.env.VITE_AUTH_API + '/users/token',
       fetchOptions,
     );
-    return userResult;
   };
   return {getUserByToken, postUser};
 };
-export {useMedia, useAuthentication, useUser};
+const useFile = () => {
+  const postFile = async (file, token) => {
+    const formData = new FormData();
+    formData.append('file', file);
+
+    const fetchOptions = {
+      method: 'POST',
+      headers: {
+        Authorization: 'Bearer: ' + token,
+      },
+      mode: 'cors',
+      body: formData,
+    };
+
+    return await fetchData(
+      import.meta.env.VITE_UPLOAD_SERVER + '/upload',
+      fetchOptions,
+    );
+  };
+
+  return {postFile};
+};
+export {useMedia, useAuthentication, useUser, useFile};
